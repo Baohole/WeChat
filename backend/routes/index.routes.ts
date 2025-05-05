5d1b8c9b7cddf394a487d3f1b5c59635aa8bbdb4
Hồ Lê Viết Bảo <hovietbao28102004@gmail.com>
2025-05-05 13:55:04 +0700

    re-build backend

--- backend/routes/index.routes.ts
+++ backend/routes/index.routes.ts
@@ -11,10 +11,13 @@ import * as AuthMdlw from '../middleware/auth.mdlw'
 import sysApi from "../config/apilink";
 
 export default (app: Express) => {
+    app.get("/start-server", (req, res) => {
+        res.send("Welcome to WeChat!!!");
+    });
+
     app.use(`${sysApi}/auth`, AuthRoutes);
     app.use(`${sysApi}/user`, UserRoutes);
     app.use(`${sysApi}/friends`, AuthMdlw.protect, FriendRoutes);
     app.use(`${sysApi}/conversation`, AuthMdlw.protect, ConvoRoutes);
     app.use(`${sysApi}/message`, AuthMdlw.protect, MessRoutes)
-
 }
